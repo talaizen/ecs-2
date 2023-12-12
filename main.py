0915fe4dbd0128425032b4fff29f291204586d35
tal.aizenkraft <tal.aizenkraft@tufin.com>
2023-12-12 13:27:48 +0200

    Add startup and shutdown actions

--- main.py
+++ main.py
@@ -1,44 +1,54 @@
-from fastapi import FastAPI, Request, Form
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 
-from forms.jason_forms import LoginForm 
+from utils.mongo_db import MongoDB
+from forms.jason_forms import LoginForm, MasterAccount
 
 
-app = FastAPI()
+MONGO_URL = "mongodb://admin:password@localhost:27017"
+MONGO_DB_NAME = "ecs"
+mongo = MongoDB(MONGO_URL, MONGO_DB_NAME)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("starting app")
+    yield
+    print("closing mongo connection")
+    mongo.close_session()
+
+
+app = FastAPI(lifespan=lifespan)
 templates = Jinja2Templates(directory="templates")
 
 
-# Mount the 'templates' and 'static' folders as a static directories
+# Mount folders as static directories
 app.mount("/templates", StaticFiles(directory="templates"), name="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/vendor", StaticFiles(directory="vendor"), name="vendor")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # You might want to restrict this in production
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 # Serve the index.html file
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse(
-        "index.html", {"request": request, "username": "tal", "age": 22}
-    )
+    return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.post("/login")
 async def login(login_data: LoginForm):
-    # TODO: Check the username and password against MongoDB
-    # If the credentials are valid, return a success response
-    # Otherwise, raise an HTTPException with a 401 status code (Unauthorized)
     username = login_data.username
     password = login_data.password
     print(username, password)
     return {"message": "Login successful"}
 
+
+@app.post("/create_master_account")
+async def create_master_account(master_account_data: MasterAccount):
+    print(master_account_data)
+    account_dict = dict(master_account_data)
+    await mongo.insert_master_account(account_dict)
