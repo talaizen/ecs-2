1ef8015626d27b798b6f73455ae65f5a80ac0041
tal.aizenkraft <tal.aizenkraft@tufin.com>
2023-12-12 13:25:29 +0200

    Add create master account page

--- static/js/master-signup.js
+++ static/js/master-signup.js
@@ -1,14 +1,21 @@
 async function createMasterAccount() {
-    const userName = document.getElementById('userName').value;
-    const password = document.getElementById('pwd').value;
+    const firstName = document.getElementById('first-name').value;
+    const lastName = document.getElementById('last-name').value;
+    const personalID = document.getElementById('personal-id').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
 
-    const response = await fetch('/login', {
+    const response = await fetch('/create_master_account', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            username: userName,
+            first_name: firstName,
+            last_name: lastName,
+            personal_id: personalID,
+            email: email,
             password: password,
         }),
     });
@@ -22,7 +29,10 @@ async function createMasterAccount() {
     }
 }
 
-document.getElementById("showPassword").addEventListener("change", function () {
-    var passwordInput = document.getElementById("pwd");
-    passwordInput.type = this.checked ? "text" : "password";
+document.getElementById("show-password").addEventListener("change", function () {
+    let passwordInput = document.getElementById("password");
+    let confirmPasswordInput = document.getElementById("confirm-password");
+    const status = this.checked ? "text" : "password";
+    passwordInput.type = status;
+    confirmPasswordInput.type = status;
   });
