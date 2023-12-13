e432449ea98840c79cffdc9cfc851214187893bc
tal.aizenkraft <tal.aizenkraft@tufin.com>
2023-12-13 17:02:17 +0200

    Update master-signup.js

--- static/js/master-signup.js
+++ static/js/master-signup.js
@@ -1,4 +1,11 @@
-async function createMasterAccount() {
+document.getElementById("show-password").addEventListener("change", function () {
+    let passwordInput = document.getElementById("password");
+    let confirmPasswordInput = document.getElementById("confirm-password");
+    let masterPasswordInput = document.getElementById("master-password");
+    passwordInput.type = confirmPasswordInput.type = masterPasswordInput.type = this.checked ? "text" : "password";
+});
+
+function prepareRequestData() {
     const firstName = document.getElementById('first-name').value;
     const lastName = document.getElementById('last-name').value;
     const personalID = document.getElementById('personal-id').value;
@@ -7,41 +14,60 @@ async function createMasterAccount() {
     const confirmPassword = document.getElementById('confirm-password').value;
     const masterPassword = document.getElementById('master-password').value;
 
-    const response = await fetch('/create_master_account', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            first_name: firstName,
-            last_name: lastName,
-            personal_id: personalID,
-            email: email,
-            password: password,
-            confirm_password: confirmPassword,
-            master_password: masterPassword
-        }),
-    });
+    return {
+        first_name: firstName,
+        last_name: lastName,
+        personal_id: personalID,
+        email: email,
+        password: password,
+        confirm_password: confirmPassword,
+        master_password: masterPassword
+    };
+}
 
+async function handleResponse(response) {
     const responseData = await response.json();
-    console.log(response.status)
-    
+
     if (response.ok) {
-        // Successful login, you can redirect or perform other actions
-        console.log('created master user successfully');
-        showCreationAlert("created master user successfully");
+        console.log('Created master user successfully');
+        showCreationAlert('Created master user successfully');
     } else {
-        if(response.status == 400) {
-            const errorMessage = responseData.detail
-            console.error('master user creation failed: ', errorMessage);
-            showFailureAlert(errorMessage);    
-        }
-        else if(response.status == 422){
-            const errorDetail = responseData.detail[0];
-            const errorMessage = errorDetail.msg;
-            console.error('master user creation failed: ', errorMessage);
-            showFailureAlert(errorMessage); 
-        }
+        handleErrorResponse(response, responseData);
+    }
+}
+
+function handleErrorResponse(response, responseData) {
+    if (response.status === 400) {
+        const errorMessage = responseData.detail;
+        console.error('Master user creation failed:', errorMessage);
+        showFailureAlert(errorMessage);
+    } else if (response.status === 422) {
+        const errorDetail = responseData.detail[0];
+        const errorMessage = errorDetail.msg;
+        console.error('Master user creation failed:', errorMessage);
+        showFailureAlert(errorMessage);
+    } else {
+        console.error('Unexpected error:', response.statusText);
+        showFailureAlert('An unexpected error occurred.');
+    }
+}
+
+async function createMasterAccount() {
+    const requestData = prepareRequestData();
+
+    try {
+        const response = await fetch('/create_master_account', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestData),
+        });
+    
+        handleResponse(response);
+    } catch (error) {
+        console.error('Error creating master user:', error);
+        showFailureAlert('An unexpected error occurred.');
     }
 }
 
@@ -53,19 +79,10 @@ function showCreationAlert(message){
 
 function showFailureAlert(message) {
     document.getElementById('alert-text').innerText = message;
+    document.getElementById('alert-container').style.backgroundColor = 'red';
     document.getElementById('alert-container').style.display = 'block';
 }
   
 function closeAlert() {
 document.getElementById('alert-container').style.display = 'none';
 }
-  
-document.getElementById("show-password").addEventListener("change", function () {
-    let passwordInput = document.getElementById("password");
-    let confirmPasswordInput = document.getElementById("confirm-password");
-    let masterPasswordInput = document.getElementById("master-password");
-    const status = this.checked ? "text" : "password";
-    passwordInput.type = status;
-    confirmPasswordInput.type = status;
-    masterPasswordInput.type = status;
-});
