1440e0c15d60b32b9219d4fc681e1a535c38c3ca
tal.aizenkraft <tal.aizenkraft@tufin.com>
2023-12-18 18:53:54 +0200

    added docmentation

--- utils/mongo_db.py
+++ utils/mongo_db.py
@@ -5,71 +5,186 @@ from .pydantic_forms import MasterAccount
 
 
 class MongoDB:
+    """
+    MongoDB class for handling operations related to the MongoDB database.
+
+    Attributes:
+        MASTER_USERS_COLLECTION (str): Name of the collection for master users.
+        CLIENT_USERS_COLLECTION (str): Name of the collection for client users.
+    """
+
     MASTER_USERS_COLLECTION = "master_users"
     CLIENT_USERS_COLLECTION = "client_users"
 
     def __init__(self, db_url: str, db_name: str) -> None:
+        """
+        Initialize the MongoDB client and select the database.
+
+        Args:
+            db_url (str): URL to the MongoDB server.
+            db_name (str): Name of the database to use.
+        """
         self.client = AsyncIOMotorClient(db_url)
         self.db = self.client[db_name]
 
     @property
     def master_users_collection(self):
+        """
+        Provides access to the master users collection in the database.
+
+        Returns:
+            Collection: The MongoDB collection for master users.
+        """
         return self.db[self.MASTER_USERS_COLLECTION]
 
     @property
     def client_users_collection(self):
+        """
+        Provides access to the client users collection in the database.
+
+        Returns:
+            Collection: The MongoDB collection for client users.
+        """
         return self.db[self.CLIENT_USERS_COLLECTION]
 
     async def is_master_password(self, password: str) -> bool:
+        """
+        Checks if the given password matches any master user's password or a master password.
+
+        Args:
+            password (str): The password to check.
+
+        Returns:
+            bool: True if the password matches, False otherwise.
+        """
         MASTER_PASSWORD = "Aizen"
         document = await self.master_users_collection.find_one({"password": password})
         return document is not None or password == MASTER_PASSWORD
 
     async def is_existing_master(self, personal_id) -> bool:
+        """
+        Checks if a master user with the given personal ID exists.
+
+        Args:
+            personal_id: The personal ID of the master user.
+
+        Returns:
+            bool: True if a master user with the given personal ID exists, False otherwise.
+        """
         document = await self.master_users_collection.find_one(
             {"personal_id": personal_id}
         )
         return document is not None
 
     async def is_existing_client(self, personal_id) -> bool:
+        """
+        Checks if a client user with the given personal ID exists.
+
+        Args:
+            personal_id: The personal ID of the client user.
+
+        Returns:
+            bool: True if a client user with the given personal ID exists, False otherwise.
+        """
         document = await self.client_users_collection.find_one(
             {"personal_id": personal_id}
         )
         return document is not None
 
     async def insert_master_account(self, account_data: MasterAccount) -> ObjectId:
+        """
+        Inserts a new master account into the database.
+
+        Args:
+            account_data (MasterAccount): The master account data to insert.
+
+        Returns:
+            ObjectId: The ObjectId of the newly inserted master account.
+        """
         result = await self.master_users_collection.insert_one(account_data)
         print(f"result id: {result.inserted_id}, {type(result.inserted_id)}")
         return result.inserted_id
 
     async def insert_client_account(self, account_data: MasterAccount) -> ObjectId:
+        """
+        Inserts a new client account into the database.
+
+        Args:
+            account_data (MasterAccount): The client account data to insert.
+
+        Returns:
+            ObjectId: The ObjectId of the newly inserted client account.
+        """
         result = await self.client_users_collection.insert_one(account_data)
         print(f"result id: {result.inserted_id}, {type(result.inserted_id)}")
         return result.inserted_id
 
     async def login_master(self, personal_id: int, password: str) -> bool:
+        """
+        Validates login credentials for a master user.
+
+        Args:
+            personal_id (int): The personal ID of the master user.
+            password (str): The password of the master user.
+
+        Returns:
+            bool: True if credentials are valid, False otherwise.
+        """
         document = await self.master_users_collection.find_one(
             {"personal_id": personal_id, "password": password}
         )
         return document is not None
 
     async def login_client(self, personal_id: int, password: str) -> bool:
+        """
+        Validates login credentials for a client user.
+
+        Args:
+            personal_id (int): The personal ID of the client user.
+            password (str): The password of the client user.
+
+        Returns:
+            bool: True if credentials are valid, False otherwise.
+        """
         document = await self.client_users_collection.find_one(
             {"personal_id": personal_id, "password": password}
         )
         return document is not None
 
     async def get_master_user(self, personal_id: int, password: str) -> bool:
+        """
+        Retrieves a master user from the database based on personal ID and password.
+
+        Args:
+            personal_id (int): The personal ID of the master user.
+            password (str): The password of the master user.
+
+        Returns:
+            Document: The document of the master user if found, None otherwise.
+        """
         document = await self.master_users_collection.find_one(
             {"personal_id": personal_id, "password": password}
         )
         return document
 
     async def get_client_user(self, personal_id: int, password: str) -> bool:
+        """
+        Retrieves a client user from the database based on personal ID and password.
+
+        Args:
+            personal_id (int): The personal ID of the client user.
+            password (str): The password of the client user.
+
+        Returns:
+            Document: The document of the client user if found, None otherwise.
+        """
         document = await self.client_users_collection.find_one(
             {"personal_id": personal_id, "password": password}
         )
         return document
 
     def close_session(self):
+        """
+        Closes the MongoDB client session.
+        """
         self.client.close()
--- utils/pydantic_forms.py
+++ utils/pydantic_forms.py
@@ -2,10 +2,30 @@ from pydantic import BaseModel, field_validator, model_validator
 
 
 class TokenResponse(BaseModel):
+    """
+    Pydantic model representing a token response.
+
+    Attributes:
+        redirect_url (str): URL to redirect the user to after token generation.
+    """
+
     redirect_url: str
 
 
 class User(BaseModel):
+    """
+    Pydantic model for a user.
+
+    Attributes:
+        type (str): Type of the user (e.g., 'master', 'client').
+        full_name (str): Full name of the user.
+        personal_id (int): Personal identification number.
+        password (str): User's password.
+        email (str): Email address of the user.
+        palga (str, optional): Additional attribute for the user, defaults to None.
+        team (str, optional): Team to which the user belongs, defaults to None.
+    """
+
     type: str
     full_name: str
     personal_id: int
@@ -16,18 +36,51 @@ class User(BaseModel):
 
 
 class LoginForm(BaseModel):
+    """
+    Pydantic model for login form data.
+
+    Attributes:
+        personal_id (int): Personal identification number.
+        password (str): Password for login.
+    """
+
     personal_id: int
     password: str
 
     @field_validator("personal_id", "password")
     @classmethod
     def not_empty(cls, v: str) -> str:
+        """
+        Field validator to ensure the field is not empty.
+
+        Args:
+            v (str): Value to be validated.
+
+        Raises:
+            ValueError: If the field is empty.
+
+        Returns:
+            str: Validated value.
+        """
         if v == "":
             raise ValueError("all fields are required")
         return v
 
 
 class MasterAccount(BaseModel):
+    """
+    Pydantic model for a master account.
+
+    Attributes:
+        first_name (str): First name of the master account holder.
+        last_name (str): Last name of the master account holder.
+        personal_id (int): Personal identification number.
+        email (str): Email address.
+        password (str): Password for the account.
+        confirm_password (str): Confirmation of the password.
+        master_password (str): Master password for additional security.
+    """
+
     first_name: str
     last_name: str
     personal_id: int
@@ -72,6 +125,21 @@ class MasterAccount(BaseModel):
 
 
 class ClientAccount(BaseModel):
+    """
+    Pydantic model for a client account.
+
+    Attributes:
+        palga (str): Attribute specific to client accounts.
+        team (str): Team name or identifier.
+        first_name (str): First name of the client account holder.
+        last_name (str): Last name of the client account holder.
+        personal_id (int): Personal identification number.
+        email (str): Email address.
+        password (str): Password for the account.
+        confirm_password (str): Confirmation of the password.
+        master_password (str): Master password for additional security.
+    """
+
     palga: str
     team: str
     first_name: str
