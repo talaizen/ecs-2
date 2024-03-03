528877b9df3ef8b30ed5752a354db6179c246524
talaizen <talkraft@gmail.com>
2024-03-03 15:57:09 +0200

    added clients ability to watch others signings

--- routes/client_routes/tables_routes.py
+++ routes/client_routes/tables_routes.py
@@ -43,6 +43,19 @@ async def client_signings(request: Request, mongo_db: MongoDB = Depends(get_mong
     )
 
 
+@router.get("/client/all_signings", response_class=HTMLResponse)
+async def client_signings(request: Request, mongo_db: MongoDB = Depends(get_mongo_db)):
+    try:
+        user = await get_current_client_user(request, mongo_db)
+    except (ValueError, HTTPException):
+        return RedirectResponse(url="/", status_code=status.HTTP_302_FOUND)
+
+    return templates.TemplateResponse(
+        "/client_user/client_all_signings.html",
+        {"request": request, "username": user.full_name},
+    )
+
+
 @router.get("/client/switch_requests", response_class=HTMLResponse)
 async def client_switch_requests(
     request: Request, mongo_db: MongoDB = Depends(get_mongo_db)
